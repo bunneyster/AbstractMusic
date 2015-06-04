bdbcccb98471cbe95143f60e267eaffc28b7c61b
Staphany Park <stapark008@gmail.com>
2015-06-04 11:51:14 -0400

    Move 'belongs_to' association to subclass.

--- app/models/composition_base.rb
+++ app/models/composition_base.rb
@@ -1,6 +1,3 @@
 class CompositionBase < ActiveRecord::Base
   self.abstract_class = true
-
-  belongs_to :composer
-  validates :composer, presence: true
 end
--- app/models/symphony.rb
+++ app/models/symphony.rb
@@ -2,4 +2,7 @@
 class Symphony < ActiveRecord::Base
   # Needed because we're not using STI.
   self.table_name = 'symphonies'
+
+  belongs_to :composer
+  validates :composer, presence: true
 end
